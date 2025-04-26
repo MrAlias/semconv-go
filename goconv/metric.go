0ccb21edaa237c068802bae12d88e202392b8e9d
Tyler Yahn <codingalias@gmail.com>
2025-04-26 08:02:32 -0700

    Export Go runtime metrics as observable

--- goconv/metric.go
+++ goconv/metric.go
@@ -27,24 +27,24 @@ var (
 // "go.config.gogc" semantic conventions. It represents the heap size target
 // percentage configured by the user, otherwise 100.
 type ConfigGogc struct {
-	inst metric.Int64UpDownCounter
+	inst metric.Int64ObservableUpDownCounter
 }
 
 // NewConfigGogc returns a new ConfigGogc instrument.
 func NewConfigGogc(m metric.Meter) (ConfigGogc, error) {
-	i, err := m.Int64UpDownCounter(
+	i, err := m.Int64ObservableUpDownCounter(
 	    "go.config.gogc",
 	    metric.WithDescription("Heap size target percentage configured by the user, otherwise 100."),
 	    metric.WithUnit("%"),
 	)
 	if err != nil {
-	    return ConfigGogc{inst: noop.Int64UpDownCounter{}}, err
+	    return ConfigGogc{inst: noop.Int64ObservableUpDownCounter{}}, err
 	}
 	return ConfigGogc{i}, nil
 }
 
 // Inst returns the underlying metric instrument.
-func (m ConfigGogc) Inst() metric.Int64UpDownCounter {
+func (m ConfigGogc) Inst() metric.Int64ObservableUpDownCounter {
 	return m.inst
 }
 
@@ -63,39 +63,28 @@ func (ConfigGogc) Description() string {
 	return "Heap size target percentage configured by the user, otherwise 100."
 }
 
-// Add adds incr to the existing count.
-//
-// The value range is [0.0,100.0]. Computed from `/gc/gogc:percent`.
-func (m ConfigGogc) Add(ctx context.Context, incr int64, attrs ...attribute.KeyValue) {
-	if len(attrs) == 0 {
-		m.inst.Add(ctx, incr)
-	} else {
-		m.inst.Add(ctx, incr, metric.WithAttributes(attrs...))
-	}
-}
-
 // GoroutineCount is an instrument used to record metric values conforming to the
 // "go.goroutine.count" semantic conventions. It represents the count of live
 // goroutines.
 type GoroutineCount struct {
-	inst metric.Int64UpDownCounter
+	inst metric.Int64ObservableUpDownCounter
 }
 
 // NewGoroutineCount returns a new GoroutineCount instrument.
 func NewGoroutineCount(m metric.Meter) (GoroutineCount, error) {
-	i, err := m.Int64UpDownCounter(
+	i, err := m.Int64ObservableUpDownCounter(
 	    "go.goroutine.count",
 	    metric.WithDescription("Count of live goroutines."),
 	    metric.WithUnit("{goroutine}"),
 	)
 	if err != nil {
-	    return GoroutineCount{inst: noop.Int64UpDownCounter{}}, err
+	    return GoroutineCount{inst: noop.Int64ObservableUpDownCounter{}}, err
 	}
 	return GoroutineCount{i}, nil
 }
 
 // Inst returns the underlying metric instrument.
-func (m GoroutineCount) Inst() metric.Int64UpDownCounter {
+func (m GoroutineCount) Inst() metric.Int64ObservableUpDownCounter {
 	return m.inst
 }
 
@@ -114,39 +103,28 @@ func (GoroutineCount) Description() string {
 	return "Count of live goroutines."
 }
 
-// Add adds incr to the existing count.
-//
-// Computed from `/sched/goroutines:goroutines`.
-func (m GoroutineCount) Add(ctx context.Context, incr int64, attrs ...attribute.KeyValue) {
-	if len(attrs) == 0 {
-		m.inst.Add(ctx, incr)
-	} else {
-		m.inst.Add(ctx, incr, metric.WithAttributes(attrs...))
-	}
-}
-
 // MemoryAllocated is an instrument used to record metric values conforming to
 // the "go.memory.allocated" semantic conventions. It represents the memory
 // allocated to the heap by the application.
 type MemoryAllocated struct {
-	inst metric.Int64Counter
+	inst metric.Int64ObservableCounter
 }
 
 // NewMemoryAllocated returns a new MemoryAllocated instrument.
 func NewMemoryAllocated(m metric.Meter) (MemoryAllocated, error) {
-	i, err := m.Int64Counter(
+	i, err := m.Int64ObservableCounter(
 	    "go.memory.allocated",
 	    metric.WithDescription("Memory allocated to the heap by the application."),
 	    metric.WithUnit("By"),
 	)
 	if err != nil {
-	    return MemoryAllocated{inst: noop.Int64Counter{}}, err
+	    return MemoryAllocated{inst: noop.Int64ObservableCounter{}}, err
 	}
 	return MemoryAllocated{i}, nil
 }
 
 // Inst returns the underlying metric instrument.
-func (m MemoryAllocated) Inst() metric.Int64Counter {
+func (m MemoryAllocated) Inst() metric.Int64ObservableCounter {
 	return m.inst
 }
 
@@ -165,39 +143,28 @@ func (MemoryAllocated) Description() string {
 	return "Memory allocated to the heap by the application."
 }
 
-// Add adds incr to the existing count.
-//
-// Computed from `/gc/heap/allocs:bytes`.
-func (m MemoryAllocated) Add(ctx context.Context, incr int64, attrs ...attribute.KeyValue) {
-	if len(attrs) == 0 {
-		m.inst.Add(ctx, incr)
-	} else {
-		m.inst.Add(ctx, incr, metric.WithAttributes(attrs...))
-	}
-}
-
 // MemoryAllocations is an instrument used to record metric values conforming to
 // the "go.memory.allocations" semantic conventions. It represents the count of
 // allocations to the heap by the application.
 type MemoryAllocations struct {
-	inst metric.Int64Counter
+	inst metric.Int64ObservableCounter
 }
 
 // NewMemoryAllocations returns a new MemoryAllocations instrument.
 func NewMemoryAllocations(m metric.Meter) (MemoryAllocations, error) {
-	i, err := m.Int64Counter(
+	i, err := m.Int64ObservableCounter(
 	    "go.memory.allocations",
 	    metric.WithDescription("Count of allocations to the heap by the application."),
 	    metric.WithUnit("{allocation}"),
 	)
 	if err != nil {
-	    return MemoryAllocations{inst: noop.Int64Counter{}}, err
+	    return MemoryAllocations{inst: noop.Int64ObservableCounter{}}, err
 	}
 	return MemoryAllocations{i}, nil
 }
 
 // Inst returns the underlying metric instrument.
-func (m MemoryAllocations) Inst() metric.Int64Counter {
+func (m MemoryAllocations) Inst() metric.Int64ObservableCounter {
 	return m.inst
 }
 
@@ -216,39 +183,28 @@ func (MemoryAllocations) Description() string {
 	return "Count of allocations to the heap by the application."
 }
 
-// Add adds incr to the existing count.
-//
-// Computed from `/gc/heap/allocs:objects`.
-func (m MemoryAllocations) Add(ctx context.Context, incr int64, attrs ...attribute.KeyValue) {
-	if len(attrs) == 0 {
-		m.inst.Add(ctx, incr)
-	} else {
-		m.inst.Add(ctx, incr, metric.WithAttributes(attrs...))
-	}
-}
-
 // MemoryGCGoal is an instrument used to record metric values conforming to the
 // "go.memory.gc.goal" semantic conventions. It represents the heap size target
 // for the end of the GC cycle.
 type MemoryGCGoal struct {
-	inst metric.Int64UpDownCounter
+	inst metric.Int64ObservableUpDownCounter
 }
 
 // NewMemoryGCGoal returns a new MemoryGCGoal instrument.
 func NewMemoryGCGoal(m metric.Meter) (MemoryGCGoal, error) {
-	i, err := m.Int64UpDownCounter(
+	i, err := m.Int64ObservableUpDownCounter(
 	    "go.memory.gc.goal",
 	    metric.WithDescription("Heap size target for the end of the GC cycle."),
 	    metric.WithUnit("By"),
 	)
 	if err != nil {
-	    return MemoryGCGoal{inst: noop.Int64UpDownCounter{}}, err
+	    return MemoryGCGoal{inst: noop.Int64ObservableUpDownCounter{}}, err
 	}
 	return MemoryGCGoal{i}, nil
 }
 
 // Inst returns the underlying metric instrument.
-func (m MemoryGCGoal) Inst() metric.Int64UpDownCounter {
+func (m MemoryGCGoal) Inst() metric.Int64ObservableUpDownCounter {
 	return m.inst
 }
 
@@ -267,39 +223,28 @@ func (MemoryGCGoal) Description() string {
 	return "Heap size target for the end of the GC cycle."
 }
 
-// Add adds incr to the existing count.
-//
-// Computed from `/gc/heap/goal:bytes`.
-func (m MemoryGCGoal) Add(ctx context.Context, incr int64, attrs ...attribute.KeyValue) {
-	if len(attrs) == 0 {
-		m.inst.Add(ctx, incr)
-	} else {
-		m.inst.Add(ctx, incr, metric.WithAttributes(attrs...))
-	}
-}
-
 // MemoryLimit is an instrument used to record metric values conforming to the
 // "go.memory.limit" semantic conventions. It represents the go runtime memory
 // limit configured by the user, if a limit exists.
 type MemoryLimit struct {
-	inst metric.Int64UpDownCounter
+	inst metric.Int64ObservableUpDownCounter
 }
 
 // NewMemoryLimit returns a new MemoryLimit instrument.
 func NewMemoryLimit(m metric.Meter) (MemoryLimit, error) {
-	i, err := m.Int64UpDownCounter(
+	i, err := m.Int64ObservableUpDownCounter(
 	    "go.memory.limit",
 	    metric.WithDescription("Go runtime memory limit configured by the user, if a limit exists."),
 	    metric.WithUnit("By"),
 	)
 	if err != nil {
-	    return MemoryLimit{inst: noop.Int64UpDownCounter{}}, err
+	    return MemoryLimit{inst: noop.Int64ObservableUpDownCounter{}}, err
 	}
 	return MemoryLimit{i}, nil
 }
 
 // Inst returns the underlying metric instrument.
-func (m MemoryLimit) Inst() metric.Int64UpDownCounter {
+func (m MemoryLimit) Inst() metric.Int64ObservableUpDownCounter {
 	return m.inst
 }
 
@@ -318,40 +263,28 @@ func (MemoryLimit) Description() string {
 	return "Go runtime memory limit configured by the user, if a limit exists."
 }
 
-// Add adds incr to the existing count.
-//
-// Computed from `/gc/gomemlimit:bytes`. This metric is excluded if the limit
-// obtained from the Go runtime is math.MaxInt64.
-func (m MemoryLimit) Add(ctx context.Context, incr int64, attrs ...attribute.KeyValue) {
-	if len(attrs) == 0 {
-		m.inst.Add(ctx, incr)
-	} else {
-		m.inst.Add(ctx, incr, metric.WithAttributes(attrs...))
-	}
-}
-
 // MemoryUsed is an instrument used to record metric values conforming to the
 // "go.memory.used" semantic conventions. It represents the memory used by the Go
 // runtime.
 type MemoryUsed struct {
-	inst metric.Int64UpDownCounter
+	inst metric.Int64ObservableCounter
 }
 
 // NewMemoryUsed returns a new MemoryUsed instrument.
 func NewMemoryUsed(m metric.Meter) (MemoryUsed, error) {
-	i, err := m.Int64UpDownCounter(
+	i, err := m.Int64ObservableCounter(
 	    "go.memory.used",
 	    metric.WithDescription("Memory used by the Go runtime."),
 	    metric.WithUnit("By"),
 	)
 	if err != nil {
-	    return MemoryUsed{inst: noop.Int64UpDownCounter{}}, err
+	    return MemoryUsed{inst: noop.Int64ObservableCounter{}}, err
 	}
 	return MemoryUsed{i}, nil
 }
 
 // Inst returns the underlying metric instrument.
-func (m MemoryUsed) Inst() metric.Int64UpDownCounter {
+func (m MemoryUsed) Inst() metric.Int64ObservableCounter {
 	return m.inst
 }
 
@@ -370,26 +303,6 @@ func (MemoryUsed) Description() string {
 	return "Memory used by the Go runtime."
 }
 
-// Add adds incr to the existing count.
-//
-// All additional attrs passed are included in the recorded value.
-//
-// Computed from
-// `(/memory/classes/total:bytes - /memory/classes/heap/released:bytes)`.
-func (m MemoryUsed) Add(
-	ctx context.Context,
-	incr int64,
-	attrs ...attribute.KeyValue,
-) {
-	m.inst.Add(
-		ctx,
-		incr,
-		metric.WithAttributes(
-			attrs...,
-		),
-	)
-}
-
 // AttrMemoryType returns an optional attribute for the "go.memory.type" semantic
 // convention. It represents the type of memory.
 func (MemoryUsed) AttrMemoryType(val MemoryTypeAttr) attribute.KeyValue {
@@ -400,24 +313,24 @@ func (MemoryUsed) AttrMemoryType(val MemoryTypeAttr) attribute.KeyValue {
 // "go.processor.limit" semantic conventions. It represents the number of OS
 // threads that can execute user-level Go code simultaneously.
 type ProcessorLimit struct {
-	inst metric.Int64UpDownCounter
+	inst metric.Int64ObservableUpDownCounter
 }
 
 // NewProcessorLimit returns a new ProcessorLimit instrument.
 func NewProcessorLimit(m metric.Meter) (ProcessorLimit, error) {
-	i, err := m.Int64UpDownCounter(
+	i, err := m.Int64ObservableUpDownCounter(
 	    "go.processor.limit",
 	    metric.WithDescription("The number of OS threads that can execute user-level Go code simultaneously."),
 	    metric.WithUnit("{thread}"),
 	)
 	if err != nil {
-	    return ProcessorLimit{inst: noop.Int64UpDownCounter{}}, err
+	    return ProcessorLimit{inst: noop.Int64ObservableUpDownCounter{}}, err
 	}
 	return ProcessorLimit{i}, nil
 }
 
 // Inst returns the underlying metric instrument.
-func (m ProcessorLimit) Inst() metric.Int64UpDownCounter {
+func (m ProcessorLimit) Inst() metric.Int64ObservableUpDownCounter {
 	return m.inst
 }
 
@@ -434,17 +347,6 @@ func (ProcessorLimit) Unit() string {
 // Description returns the semantic convention description of the instrument
 func (ProcessorLimit) Description() string {
 	return "The number of OS threads that can execute user-level Go code simultaneously."
-}
-
-// Add adds incr to the existing count.
-//
-// Computed from `/sched/gomaxprocs:threads`.
-func (m ProcessorLimit) Add(ctx context.Context, incr int64, attrs ...attribute.KeyValue) {
-	if len(attrs) == 0 {
-		m.inst.Add(ctx, incr)
-	} else {
-		m.inst.Add(ctx, incr, metric.WithAttributes(attrs...))
-	}
 }
 
 // ScheduleDuration is an instrument used to record metric values conforming to
